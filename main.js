6d33c77dc07670939a2ca2d62dd503bdeb3113d9
1ghao <linwanghao2007@gmail.com>
2023-12-13 21:44:13 +0100

    commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const startMinutes = 25;
+let time = startMinutes * 60;
+
+const countdownParragraph = document.getElementById("countdown");
+
+setInterval(updateCountdown, 1000)
+
+function updateCountdown() {
+    const minutes = Math.floor(time/60);
+    let seconds = time % 60;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    countdownParragraph.innerHTML = `${minutes}:${seconds}`;
+    time--;
+}
