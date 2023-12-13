0169cfec35bc724e5d30c7bac37dee13a3ffe3f5
1ghao <linwanghao2007@gmail.com>
2023-12-13 22:04:08 +0100

    commit

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
-const startMinutes = 25;
+const startMinutes = 0.05;
 let time = startMinutes * 60;
 
 const countdownParragraph = document.getElementById("countdown");
 
 setInterval(updateCountdown, 1000)
 
-function updateCountdown() {
-    const minutes = Math.floor(time/60);
-    let seconds = time % 60;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-
-    countdownParragraph.innerHTML = `${minutes}:${seconds}`;
-    time--;
+function playAlarm() {
+    let alarmSound = new Audio("alarm.wav");
+    alarmSound.play();
 }
+
+function updateCountdown() {
+    if (time >= 0) {
+        const minutes = Math.floor(time/60);
+        let seconds = time % 60;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        countdownParragraph.innerHTML = `${minutes}:${seconds}`;
+        time--;
+    } else {
+        setInterval(playAlarm, 1000);
+        countdownParragraph.innerHTML = "Time's up ⏰⏰⏰";
+    }
+}
+
+
